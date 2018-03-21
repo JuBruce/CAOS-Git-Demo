0bb0c3c68a578bd3663ee55147f8f31956502c5f
root <root@localhost.localdomain>
2018-03-21 16:55:47 -0500

    lol

--- git-file.cpp
+++ git-file.cpp
@@ -5,8 +5,8 @@ using namespace std;
 int main(int argc, const char * argv[]) {
     string lol;
     cout << "Hello, World!\n";
-    cout << "Justin Bruce";
-    cout << "some loser named Megan Ramaker";
+    cout << "Justin Bruce\n";
+    cout << "some loser named Megan Ramaker\n";
 	cout << "the most 1337 haxor Xinx9\n\nTell me how you feel: ";	
     cin >> lol;
     cout << lol << endl;
