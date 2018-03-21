796ead621494305c7b130b05d4623ce08b63a12e
root <root@localhost.localdomain>
2018-03-21 16:48:32 -0500

    uhhhhh i didn't do it

--- git-file.cpp
+++ git-file.cpp
@@ -1,10 +1,14 @@
 #include <iostream>
+#include <string>
 using namespace std;
 
 int main(int argc, const char * argv[]) {
-    
+    string lol;
     cout << "Hello, World!\n";
     cout << "Justin Bruce";
-    cout << "Megan Ramaker";
-    return 0;
+    cout << "some loser named Megan Ramaker";
+	cout << "the most 1337 haxor Xinx9\n\nTell me how you feel: ";	
+    cin >> lol;
+    cout << lol << endl;
+	return 0;
 }
