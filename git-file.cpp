ea56a49293842ec95bd3e968a716051e0d1d08d0
Jared M Schooley <jaschoo@siue.edu>
2018-03-21 17:40:15 -0500

    Fixed broken comments

--- git-file.cpp
+++ git-file.cpp
@@ -5,24 +5,17 @@ using namespace std;
 int main(int argc, const char * argv[]) {
     string lol;
     cout << "Hello, World!\n";
-<<<<<<< HEAD
-<<<<<<< HEAD
     cout << "Justin Bruce";
     cout << "some loser named Megan Ramaker";
-	cout << "the most 1337 haxor Xinx9\n\nTell me how you feel: ";	
-cout 
-cin >> lol;
-=======
-=======
-    cout << "Jesse Rogers III";
->>>>>>> 55690567d359eeb7ec740b59bafdb78327004c5f
+    cout << "the most 1337 haxor Xinx9\n\nTell me how you feel: "; 
+    cin >> lol; 
+    cout << "Jesse Rogers III"; 
     cout << "Thomas Clifford\n";
     cout << "Justin Bruce SUCKS!!!!";
     cout << "Justin Bruce\n";
     cout << "Burke rides the special bus\n";
-	  cout << "\nTell me how you feel: ";	
+    cout << "\nTell me how you feel: ";	
     cin >> lol;
->>>>>>> refs/remotes/origin/dev
     cout << lol << endl;
 	  return 0;
 }
