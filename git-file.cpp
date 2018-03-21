d20f9be84584c50dea980f9cd748ea76f99602e0
crusty512 <crusty512@yahoo.com>
2018-03-21 17:15:31 -0500

    added a fluffy bunny to repo

--- git-file.cpp
+++ git-file.cpp
@@ -7,7 +7,9 @@ int main(int argc, const char * argv[]) {
     cout << "Hello, World!\n";
     cout << "Justin Bruce";
     cout << "some loser named Megan Ramaker";
+    cout << "Michael Bracy\n";
 	cout << "the most 1337 haxor Xinx9\n\nTell me how you feel: ";	
+   
     cin >> lol;
     cout << lol << endl;
 	return 0;
